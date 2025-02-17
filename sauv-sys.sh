245576b107a12e1fdb6cd56bba341bea7d94233f
estemobs <estemobsgaming@gmail.com>
2025-02-17 13:43:38 +0100

    Add option to empty trash before backup and update backup paths to use USER_HOME

--- sauv-sys.sh
+++ sauv-sys.sh
@@ -11,9 +11,19 @@ log() {
     echo "$(date) - $1" | tee -a "$LOG_FILE"
 }
 
+# Demander à l'utilisateur s'il veut vider les corbeilles
+read -p "Voulez-vous vider toutes les corbeilles avant de commencer la sauvegarde ? (oui/non) " response
+if [[ "$response" == "oui" ]]; then
+    log "Vidage des corbeilles..."
+    rm -rf $USER_HOME/.local/share/Trash/* && log "Corbeille de $USER_HOME vidée."
+    sudo rm -rf /root/.local/share/Trash/* && log "Corbeille de root vidée."
+else
+    log "Les corbeilles ne seront pas vidées."
+fi
+
 # Sauvegarde des paquets installés
 log "Sauvegarde des paquets..."
-dpkg --get-selections > "$BACKUP_DIR/apt-packages.txt" && log "Sauvegarde des paquets APT réussie."
+comm -23 <(apt-mark showmanual | sort) <(zcat /usr/share/doc/ubuntu-minimal/ubuntu-minimal.list.gz | sort) > "$BACKUP_DIR/apt-packages.txt" && log "Sauvegarde des paquets APT réussie."
 flatpak list --app --columns=application > "$BACKUP_DIR/flatpak-apps.txt" && log "Sauvegarde des paquets Flatpak réussie."
 snap list > "$BACKUP_DIR/snap-packages.txt" && log "Sauvegarde des paquets Snap réussie."
 
@@ -31,32 +41,26 @@ fi
 
 # Sauvegarde des configurations et fichiers importants
 log "Sauvegarde des configurations..."
-[ -d "$HOME/.config" ] && cp -r "$HOME/.config" "$BACKUP_DIR/config" && log "Sauvegarde de .config réussie."
-[ -f "$HOME/.bashrc" ] && cp "$HOME/.bashrc" "$BACKUP_DIR/.bashrc" && log "Sauvegarde de .bashrc réussie."
-[ -f "$HOME/.zshrc" ] && cp "$HOME/.zshrc" "$BACKUP_DIR/.zshrc" && log "Sauvegarde de .zshrc réussie."
-[ -d "$HOME/.local/share" ] && cp -r "$HOME/.local/share" "$BACKUP_DIR/.local_share" && log "Sauvegarde de .local/share réussie."
-[ -d "$HOME/.gnupg" ] && cp -r "$HOME/.gnupg" "$BACKUP_DIR/.gnupg" && log "Sauvegarde de .gnupg réussie."
-[ -d "$HOME/.ssh" ] && cp -r "$HOME/.ssh" "$BACKUP_DIR/ssh" && log "Sauvegarde de .ssh réussie."
+[ -d "$USER_HOME/.config" ] && cp -r "$USER_HOME/.config" "$BACKUP_DIR/config" && log "Sauvegarde de .config réussie."
+[ -f "$USER_HOME/.bashrc" ] && cp "$USER_HOME/.bashrc" "$BACKUP_DIR/.bashrc" && log "Sauvegarde de .bashrc réussie."
+[ -f "/root/.bashrc" ] && cp "/root/.bashrc" "$BACKUP_DIR/root.bashrc" && log "Sauvegarde de root.bashrc réussie."
+[ -d "$USER_HOME/.local/share" ] && cp -r "$USER_HOME/.local/share" "$BACKUP_DIR/.local_share" && log "Sauvegarde de .local/share réussie."
+[ -d "$USER_HOME/.gnupg" ] && cp -r "$USER_HOME/.gnupg" "$BACKUP_DIR/.gnupg" && log "Sauvegarde de .gnupg réussie."
+[ -d "$USER_HOME/.ssh" ] && cp -r "$USER_HOME/.ssh" "$BACKUP_DIR/ssh" && log "Sauvegarde de .ssh réussie."
 
 # Sauvegarde des extensions et paramètres GNOME
 log "Sauvegarde des extensions et paramètres GNOME..."
-[ -d "$HOME/.local/share/gnome-shell/extensions/" ] && cp -r "$HOME/.local/share/gnome-shell/extensions/" "$BACKUP_DIR/gnome-shell-extensions/" && log "Sauvegarde des extensions GNOME réussie."
+[ -d "$USER_HOME/.local/share/gnome-shell/extensions/" ] && cp -r "$USER_HOME/.local/share/gnome-shell/extensions/" "$BACKUP_DIR/gnome-shell-extensions/" && log "Sauvegarde des extensions GNOME réussie."
 dconf dump /org/gnome/ > "$BACKUP_DIR/gnome-settings.dconf" && log "Sauvegarde des paramètres GNOME réussie."
 
 # Sauvegarde des thèmes et icônes
 log "Sauvegarde des thèmes et icônes..."
-[ -d "$HOME/.themes" ] && cp -r "$HOME/.themes" "$BACKUP_DIR/themes/" && log "Sauvegarde des thèmes réussie."
-[ -d "$HOME/.icons" ] && cp -r "$HOME/.icons" "$BACKUP_DIR/icons/" && log "Sauvegarde des icônes réussie."
-
-# Sauvegarde des scripts et applications
-log "Sauvegarde des scripts et applications..."
-[ -d "$HOME/bin" ] && cp -r "$HOME/bin" "$BACKUP_DIR/bin/" && log "Sauvegarde de bin réussie."
-[ -d "$HOME/scripts" ] && cp -r "$HOME/scripts" "$BACKUP_DIR/scripts/" && log "Sauvegarde de scripts réussie."
+[ -d "$USER_HOME/.themes" ] && cp -r "$USER_HOME/.themes" "$BACKUP_DIR/themes/" && log "Sauvegarde des thèmes réussie."
+[ -d "$USER_HOME/.icons" ] && cp -r "$USER_HOME/.icons" "$BACKUP_DIR/icons/" && log "Sauvegarde des icônes réussie."
 
 # Sauvegarde des fichiers personnels importants
 log "Sauvegarde des fichiers personnels..."
-[ -d "$HOME/Documents" ] && cp -r "$HOME/Documents" "$BACKUP_DIR/Documents/" && log "Sauvegarde de Documents réussie."
-[ -d "$HOME/Images" ] && cp -r "$HOME/Images" "$BACKUP_DIR/Images/" && log "Sauvegarde de Images réussie."
+cp -r /home/* "$BACKUP_DIR/home" && log "Sauvegarde de /home réussie."
 
 # Sauvegarde des crontabs
 log "Sauvegarde des crontabs..."
